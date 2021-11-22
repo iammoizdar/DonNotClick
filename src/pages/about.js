c68b83b0bbe38d748bd6f885f55c61524c0a6960
Abdul Moiz <moiz.webevis@gmail.com>
2021-11-22 17:48:19 +0500

    Added Content

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,11 @@ import { StyledPage } from "../styles/StyledPageCss"
 const SecondPage = () => (
     <Layout>
     <StyledPage>
-    <h1>Second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi in blanditiis voluptates, dolore quibusdam sequi odit itaque qui accusamus excepturi fuga dolores facere. Similique repellendus doloribus excepturi provident quisquam vitae! Praesentium nam error ipsum ad architecto laudantium, doloribus soluta enim.</p>
+    <h2>Contact Us</h2>
+    <br />
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, nihil esse facilis, maiores enim reprehenderit voluptatum minus ullam nam atque labore libero tempore iusto! A odit quas alias nemo aperiam earum quos exercitationem dolorum et quibusdam quia, odio eos temporibus iste magni voluptates non? Itaque maiores corrupti illum tenetur perferendis.</p>
     </StyledPage>
     </Layout>
 )
--- src/pages/info.js
+++ src/pages/info.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 import { StyledPage } from '../styles/StyledPageCss'
 
 const info=()=> {
-    return (<Layout> <StyledPage><h2>Welcome To Info Page</h2> </StyledPage> </Layout>)
+    return (<Layout> <StyledPage><h1>Info Page</h1> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur tempore delectus laudantium deserunt molestias non, voluptatum at amet molestiae reprehenderit accusantium impedit voluptate. Qui ipsa id magni odio ab! Quaerat esse qui rem assumenda aliquid. Omnis reiciendis aperiam quod assumenda architecto debitis temporibus voluptates neque unde odio, dolores in aspernatur quibusdam magni expedita vitae. Laboriosam officia provident recusandae. Facilis quos voluptatum, ab, magnam molestias tempore assumenda ex quia perspiciatis odit nemo inventore vitae, unde dolorum itaque minima amet officiis magni. Numquam, sed quia. Doloribus quam a, deserunt quasi, culpa fugit, ipsum dolorum dignissimos dicta recusandae dolores sed nobis! Quos, aliquam.</p> </StyledPage> </Layout>)
 }
 
 export default info
